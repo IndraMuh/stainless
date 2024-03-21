c31117084fbadf6212e7bbd6c3912dad5831c806
Vionindya <vionindyasyifa@gmail.com>
2024-03-21 09:39:32 +0700

    create signUp Page

---
+++ signUp.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Form login dan registrasi</title>
+        <link rel="stylesheet" href="signUp.css">
+    </head>
+    <body>   
+        <div>
+            <div class="container">
+            <input type="checkbox"  id="check">
+             
+            <div class="Register form">
+                <h1>REGISTER</h1>
+                <h4>NEW CUSTOMERS</h4>
+                <p>By creating an account with our store, you will be able to move through the checkout process faster, 
+                     store multiple shipping addresses, view and track your orders in your account, and more.</p>
+                    <form action="#">
+                         <input type="text" placeholder="FIRST NAME">
+                         <input type="text" placeholder="LAST NAME">
+                         <input type="text" placeholder="EMAIL ADDRESS">
+                         <input type="text" placeholder="PASSWORD">
+                         <input type="button" value="SIGN UP" class="button">
+                        </form>
+                    </div>            
+            </div>
+                
+                <div class="wrapper">
+                    <div class="login form">
+                        <h1>LOGIN</h1>
+                        <h4>HAVE AN ACCOUNT?</h4>
+                        <p>Please provide the email address and password associated with the account. 
+                            If you're having trouble, please use the prompt below to reset your password.</p>
+                            <input type="button" value="PROCEED TO LOGIN" class="button">
+                    </div>
+                </div>
+        </div>
+    </body>
