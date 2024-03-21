a150b4b266b8a224be651d7ea4828f52030cf04d
Vionindya <vionindyasyifa@gmail.com>
2024-03-21 09:20:37 +0700

    create login

---
+++ login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Form login dan registrasi</title>
+        <link rel="stylesheet" href="login.css">
+    </head>
+
+    <body>             
+        <!--Header--> 
+       <div>
+        <div class="ContentWraper">
+            <div class="container">
+             <input type="checkbox"  id="check">
+             
+                <div class="Login form">
+                 <h1>LOGIN</h1>
+                 <h4>REGISTERED CUSTOMERS</h4>
+                 <p>Please provide the email address and password associated with the account. 
+                    If you're having trouble, please use the prompt below to reset your password.</p>
+                    <form action="#">
+                        <input type="text" placeholder="Email Address">
+                        <input type="text" placeholder="Password">
+                        <input type="button" value="SIGN IN" class="button">
+                    </form>
+                </div>            
+            </div>
+        
+            <div class="wrapper">
+                <div class="register form">
+                <h1>REGISTER</h1>
+                <h4>NEED AN ACCOUNT?</h4>
+                <p>By creating an account with our store, you will be able to move through the checkout process faster, 
+                    store multiple shipping addresses, view and track your orders in your account, and more.</p>
+                    <input type="button" value="PROCEED TO REGISTER" class="button">
+                </div>
+            </div>
+    </div>
+    </body>
